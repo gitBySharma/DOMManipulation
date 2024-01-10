2cc634ee995323309e513d6ca87135b0d1e98df1
gitBySharma <fetchsubhankar@gmail.com>
2024-01-10 13:59:46 +0530

    two files added

---
+++ fruitShop.js
@@ -0,0 +1,13 @@
+const mainHeading = document.getElementById('main-heading');
+mainHeading.textContent = "Fruit World";
+mainHeading.style.color = "orange";
+
+const header = document.getElementById('header');
+header.style.backgroundColor = "green";
+header.style.borderBottom = "3px solid orange";
+
+const basket = document.getElementById('basket-heading');
+basket.style.color = "green";
+
+const thanks = document.getElementById('thanks');
+thanks.innerHTML = "<p>Please visit us again</p>";
